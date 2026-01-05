7c681414b31519eb887f1d91367369d0579b4879
Artem <artemp2208@gmail.com>
2026-01-06 01:03:15 +0200

    Solution

--- user/serializers.py
+++ user/serializers.py
@@ -1,1 +1,44 @@
-# write your code here
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=5)
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "password", "email")
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data["username"],
+            email=validated_data.get("email", ""),
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=False,
+        min_length=5,
+    )
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "password", "is_staff")
+        read_only_fields = ("is_staff",)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user

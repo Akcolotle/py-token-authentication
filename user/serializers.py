7f777a83910b9b0597c5fac455e8ac75738ef859
Artem <artemp2208@gmail.com>
2026-01-11 21:47:58 +0200

    serializer

--- user/serializers.py
+++ user/serializers.py
@@ -4,34 +4,24 @@ from rest_framework import serializers
 User = get_user_model()
 
 
-class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=5)
-
-    class Meta:
-        model = User
-        fields = ("id", "username", "password", "email")
-
-    def create(self, validated_data):
-        user = User(
-            username=validated_data["username"],
-            email=validated_data.get("email", ""),
-        )
-        user.set_password(validated_data["password"])
-        user.save()
-        return user
-
-
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True,
-        required=False,
         min_length=5,
+        required=False,
     )
 
     class Meta:
         model = User
         fields = ("id", "username", "email", "password", "is_staff")
         read_only_fields = ("is_staff",)
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
 
     def update(self, instance, validated_data):
         password = validated_data.pop("password", None)
